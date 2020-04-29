5ed1d6368cb407f743164f52b2d0876642e9e577
Antoine J <ajegou@groupe-beaumanoir.fr>
2020-04-29 19:40:19 +0200

    updateImageGraphQl

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const graphqlHttp = require('express-graphql');
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const graphqlHttp = require("express-graphql");
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(
+      null,
+      new Date().toISOString().split(":").join("-") + "-" + file.originalname
+    );
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -36,38 +41,55 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if(req.method === 'OPTIONS'){
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
 });
 
+app.use(auth);
+
+app.put("/post-image", (req, res, next) => {
+  if(!req.isAuth){
+    throw new Error('Not authenticated!')
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided!" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored.", filePath: req.file.path.replace("\\", "/")});
+});
+
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    customFormatErrorFn(err){
-      if(!err.originalError){
+    customFormatErrorFn(err) {
+      if (!err.originalError) {
         return err;
       }
       const data = err.originalError.data;
-      const message = err.message || 'An error occurred.';
+      const message = err.message || "An error occurred.";
       const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data};
-    }
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
@@ -80,10 +102,17 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    process.env.connectionMongoDB
-  )
-  .then(result => {
+  .connect(process.env.connectionMongoDB)
+  .then((result) => {
     app.listen(8080);
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, filePath);
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,32 +1,32 @@
-const bcrypt = require("bcryptjs");
-const validator = require("validator");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const User = require("../models/userModel");
-const Post = require("../models/postModel");
+const User = require('../models/userModel');
+const Post = require('../models/postModel');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     const errors = [];
     if (!validator.isEmail(userInput.email)) {
-      errors.push({ message: "E-Mail is invalid." });
+      errors.push({ message: 'E-Mail is invalid.' });
     }
     if (
       validator.isEmpty(userInput.password) ||
       !validator.isLength(userInput.password, { min: 5 })
     ) {
-      errors.push({ message: "Password too short!" });
+      errors.push({ message: 'Password too short!' });
     }
     if (errors.length > 0) {
-      const error = new Error("Invalid input");
+      const error = new Error('Invalid input');
       error.data = errors;
       error.code = 422;
       throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
-      const error = new Error("User exists already!");
+      const error = new Error('User exists already!');
       throw error;
     }
     const hashedPw = await bcrypt.hash(userInput.password, 12);
@@ -42,13 +42,13 @@ module.exports = {
   login: async function ({ email, password }, req) {
     const user = await User.findOne({ email: email });
     if (!user) {
-      const error = new Error("User not found.");
+      const error = new Error('User not found.');
       error.code = 401;
       throw error;
     }
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error("Password is incorrect.");
+      const error = new Error('Password is incorrect.');
       error.code = 401;
       throw error;
     }
@@ -58,42 +58,155 @@ module.exports = {
         email: user.email,
       },
       process.env.secret,
-      { expiresIn: "1h" }
+      { expiresIn: '1h' },
     );
     return { token: token, userId: user._id.toString() };
   },
 
   createPost: async function ({ postInput }, req) {
+    if (!req.isAuth) {
+      const erro = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
     const errors = [];
     if (
       validator.isEmpty(postInput.title) ||
       !validator.isLength(postInput.title, { min: 5 })
     ) {
-      errors.push({ message: "Title is invalid." });
+      errors.push({ message: 'Title is invalid.' });
     }
     if (
       validator.isEmpty(postInput.content) ||
       !validator.isLength(postInput.content, { min: 5 })
     ) {
-      errors.push({ message: "Content is invalid." });
+      errors.push({ message: 'Content is invalid.' });
     }
     if (errors.length > 0) {
-      const error = new Error("Invalid input");
+      const error = new Error('Invalid input');
       error.data = errors;
       error.code = 422;
+      throw error;
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error('Invalid user');
+      error.code = 401;
       throw error;
     }
     const post = new Post({
       title: postInput.title,
       content: postInput.content,
       imageUrl: postInput.imageUrl,
+      creator: user,
     });
     const createdPost = await post.save();
+    user.posts.push(createdPost);
+    await user.save();
     return {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
       createdAt: createdPost.createdAt.toISOString(),
-      updatedAt: createdPost.updatedAt.toISOString()
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
+  },
+
+  posts: async function ({ page }, req) {
+    if (!req.isAuth) {
+      const erro = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    if (!page) {
+      page = 1;
+    }
+    const perPage = 2;
+    const totalPosts = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate('creator');
+    return {
+      posts: posts.map((p) => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString(),
+        };
+      }),
+      totalPosts: totalPosts,
+    };
+  },
+
+  post: async function ({ id }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const post = await Post.findById(id).populate('creator');
+    if (!post) {
+      const error = new Error('No post found!');
+      error.code = 404;
+      throw error;
+    }
+    return {
+      ...post._doc,
+      _id: post._id.toString(),
+      createdAt: post.createdAt.toISOString(),
+      updatedAt: post.updatedAt.toISOString(),
+    };
+  },
+
+  updatePost: async function({ id, postInput }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const post = await Post.findById(id).populate('creator');
+    if (!post) {
+      const error = new Error('No post found!');
+      error.code = 404;
+      throw error;
+    }
+    if (post.creator._id.toString() !== req.userId.toString()) {
+      const error = new Error('Not authorized!');
+      error.code = 403;
+      throw error;
+    }
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid.' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid.' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    post.title = postInput.title;
+    post.content = postInput.content;
+    if (postInput.imageUrl !== 'undefined') {
+      post.imageUrl = postInput.imageUrl;
+    }
+    const updatedPost = await post.save();
+    return {
+      ...updatedPost._doc,
+      _id: updatedPost._id.toString(),
+      createdAt: updatedPost.createdAt.toISOString(),
+      updatedAt: updatedPost.updatedAt.toISOString()
     };
   },
 };
